ade20b8e2db301bdfc21078801ea7b2310c620ca
Desiree Jallow <60536540+desiree-jallow@users.noreply.github.com>
2020-07-23 16:56:08 -0500

    initial set up

---
+++ TakeMyMoney/Model/Credit.swift
@@ -0,0 +1,17 @@
+//
+//  Credit.swift
+//  TakeMyMoney
+//
+//  Created by Desiree on 7/23/20.
+//  Copyright © 2020 Desiree. All rights reserved.
+//
+
+import Foundation
+
+struct Credit {
+    var cardNumber : Int
+    var validDate : Date
+    var cvv : Int
+    var cardHolder : String
+    
+}
---
+++ TakeMyMoney/Model/Paypal.swift
@@ -0,0 +1,14 @@
+//
+//  Paypal.swift
+//  TakeMyMoney
+//
+//  Created by Desiree on 7/23/20.
+//  Copyright © 2020 Desiree. All rights reserved.
+//
+
+import Foundation
+
+struct Paypal {
+    var userName : String
+    var Password : String
+}
